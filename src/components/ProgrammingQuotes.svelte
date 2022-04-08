69945c18fc8fa9bcf008dd3c918002347b61b4cc
Fizzify <21076@brightwoods.edu.ph>
2022-04-08 12:17:05 +0800

    Added src

---
+++ src/components/ProgrammingQuotes.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let quote;
+</script>
+
+<h1>{quote.en}</h1>
+<p>Made by Fizzify</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,40 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch('https://programming-quotes-api.herokuapp.com/quotes/random');
+    const data = await res.json();
+    if (res.ok) {
+      return { 
+	props: { quote: data }
+      }
+    }
+  }
+</script>
+
+<script>
+  import ProgrammingQuotes from '../components/ProgrammingQuotes.svelte';
+  export let quote;
+</script>
+
+<section>
+  <ProgrammingQuotes {quote} />
+</section>
+
+<style>
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  section {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    font-family: Arial, sans-serif;
+  }
+</style>
